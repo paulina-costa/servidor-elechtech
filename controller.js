9bb3df77db9047f48fc4cf4cdd075a8648617c52
Rafael <rafael.silva.oliveira.tecnico@gmail.com>
2024-11-19 21:16:45 -0300

    saindo da login para a home corretamente

--- controller.js
+++ controller.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { connection } = require('./bdConfig');
+const bcrypt = require('bcrypt');
 const {
   homeRoute,
   listarRegistros,
@@ -19,20 +21,61 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json()); // Para fazer parsing de JSON no corpo da requisição
 
+// Função para gerar o token JWT
+function gerarToken(usuario) {
+  const segredo = 'seu-segredo-aqui'; // Defina seu segredo para o JWT
+  const payload = { id: usuario.id, nome: usuario.nome };
+  return jwt.sign(payload, segredo, { expiresIn: '1h' }); // Expira em 1 hora
+}
+
+// Função para verificar as credenciais
+async function verificarCredenciais(email, senha) {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM usuario WHERE email = ?';
+    connection.query(query, [email], async (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (results.length > 0) {
+          const usuario = results[0];
+          // Comparar senha (hashing)
+          const senhaValida = await bcrypt.compare(senha, usuario.password);
+          if (senhaValida) {
+            resolve(usuario);
+          } else {
+            resolve(null); // Credenciais inválidas
+          }
+        } else {
+          resolve(null); // Usuário não encontrado
+        }
+      }
+    });
+  });
+}
+
+// Rota de login
+app.post('/login', async (req, res) => {
+  const { email, senha } = req.body;
+  try {
+    const usuario = await verificarCredenciais(email, senha);
+    if (usuario) {
+      const token = gerarToken(usuario); // Gerar o token JWT
+      res.json({ token });
+    } else {
+      res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Erro no servidor. Tente novamente mais tarde.' });
+  }
+});
+
 app.get('/', homeRoute);
-
 app.get('/filtros', listarRegistros(connection));
-
-app.get('/filtros/:id', listarRegistroPorId(connection));  // Certifique-se de que o id da query está correto
-
+app.get('/filtros/:id', listarRegistroPorId(connection));  
 app.post('/chamados', criarChamado(connection));
-
 app.post('/filtros', filtrarChamados(connection));
-
 app.put('/filtros/:id', atualizarChamado(connection));
-
 app.delete('/filtros/:id', deletarChamado(connection));
-
 app.get('*', rotaNaoExistente);
 
 app.listen(port, () => {
--- utils.js
+++ utils.js
@@ -1,10 +1,52 @@
-// utils.js
 const moment = require('moment');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // Função para a rota '/'
 const homeRoute = (req, res) => {
   res.send('Somos Elechtech!');
+};
+
+// Função para verificar as credenciais e gerar o token
+const loginUsuario = (connection) => (req, res) => {
+  const { email, password } = req.body;
+
+  // Verifica se o email e a senha foram fornecidos
+  if (!email || !password) {
+    return res.status(400).json({ erro: 'Email e senha são obrigatórios.' });
+  }
+
+  const sqlCheckEmail = 'SELECT * FROM usuario WHERE email = ?';
+  connection.query(sqlCheckEmail, [email], (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar usuário:', err);
+      return res.status(500).json({ erro: 'Erro ao buscar usuário.' });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ erro: 'Credenciais inválidas.' });
+    }
+
+    // Verifica se a senha é válida
+    const user = results[0];
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        console.error('Erro ao comparar senha:', err);
+        return res.status(500).json({ erro: 'Erro interno ao comparar senha.' });
+      }
+
+      if (!isMatch) {
+        return res.status(401).json({ erro: 'Credenciais inválidas.' });
+      }
+
+      // Gera o token JWT
+      const payload = { id: user.id, nomeUsuario: user.nomeUsuario, email: user.email };
+      const token = jwt.sign(payload, 'secreta-chave-jwt', { expiresIn: '1h' });
+
+      return res.status(200).json({ message: 'Login bem-sucedido.', token });
+    });
+  });
 };
 
 // Função para listar todos os registros
@@ -19,25 +61,23 @@ const listarRegistros = (connection) => (req, res) => {
 };
 
 // Função para listar um registro por ID
-// Função para listar um registro por ID
 const listarRegistroPorId = (connection) => async (req, res) => {
   const { id } = req.params;
 
   const sql = 'SELECT * FROM abrirChamado WHERE id = ?';
 
   connection.query(sql, [id], (error, results) => {
-      if (error) {
-          console.error(error);
-          return res.status(500).json({ message: 'Erro ao buscar chamado.' });
-      }
-      if (results.length > 0) {
-          // Convertendo a data para o formato adequado antes de enviar para o front
-          const chamado = results[0];
-          chamado.datas = moment(chamado.datas).format('YYYY-MM-DD'); // Formata a data
-          return res.status(200).json(chamado);
-      } else {
-          return res.status(404).json({ message: 'Chamado não encontrado.' });
-      }
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Erro ao buscar chamado.' });
+    }
+    if (results.length > 0) {
+      const chamado = results[0];
+      chamado.datas = moment(chamado.datas).format('YYYY-MM-DD');
+      return res.status(200).json(chamado);
+    } else {
+      return res.status(404).json({ message: 'Chamado não encontrado.' });
+    }
   });
 };
 
@@ -49,7 +89,7 @@ const criarChamado = (connection) => (req, res) => {
     email, descricao
   } = req.body;
 
-  // Validações
+  // Verifica se todos os campos obrigatórios foram preenchidos
   if (!datas || !setor || !tiposDoChamado || !nivelDeUrgencia || 
       !nomeEquipamento || !FK_tecnicoResponsavelPeloChamado || !email || !descricao) {
     return res.status(400).json({ erro: 'Todos os campos são obrigatórios.' });
@@ -73,184 +113,101 @@ const criarChamado = (connection) => (req, res) => {
     return res.status(400).json({ erro: 'Nível de urgência inválido' });
   }
 
-    const sqlInsert = `
-      INSERT INTO abrirChamado (nomeUsuario, datas, setor, tiposDoChamado,
-      nivelDeUrgencia, nomeEquipamento, FK_tecnicoResponsavelPeloChamado, email, descricao)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+  const sqlInsert = `
+    INSERT INTO abrirChamado (nomeUsuario, datas, setor, tiposDoChamado,
+    nivelDeUrgencia, nomeEquipamento, FK_tecnicoResponsavelPeloChamado, email, descricao)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
 
-    const values = [nomeUsuario, datas, setor, tiposDoChamado, nivelDeUrgencia,
-                    nomeEquipamento, FK_tecnicoResponsavelPeloChamado, email, descricao];
+  const values = [nomeUsuario, datas, setor, tiposDoChamado, nivelDeUrgencia,
+                  nomeEquipamento, FK_tecnicoResponsavelPeloChamado, email, descricao];
 
-    connection.query(sqlInsert, values, (err, results) => {
-      if (err) {
-        console.error('Erro ao adicionar chamado:', err);
-        return res.status(500).send('Erro interno do servidor');
-      }
-
-      res.status(201).json({ message: 'Chamado adicionado com sucesso', chamadoId: results.insertId });
-    });
-  };
-
-// Função para filtrar chamados
-const filtrarChamados = (connection) => (req, res) => {
-  const { 'tipo-chamado': tiposDoChamado, 'nivel-urgencia': nivelDeUrgencia, setor, equipamento: nomeEquipamento } = req.body;
-  const { datas, resolucao, FK_tecnicoResponsavelPeloChamado, orderByDate } = req.body;
-
-  const filters = [
-    { field: 'setor', value: setor },
-    { field: 'tiposDoChamado', value: tiposDoChamado },
-    { field: 'nivelDeUrgencia', value: nivelDeUrgencia },
-    { field: 'nomeEquipamento', value: nomeEquipamento },
-    { field: 'resolucao', value: resolucao },
-    { field: 'FK_tecnicoResponsavelPeloChamado', value: FK_tecnicoResponsavelPeloChamado }
-  ];
-
-  let sql = 'SELECT * FROM abrirChamado WHERE 1=1';
-  const values = [];
-
-  filters.forEach(filter => {
-    if (filter.value) {
-      sql += ` AND ${filter.field} = ?`;
-      values.push(filter.value);
-    }
-  });
-
-  // Lógica para filtrar pela data
-  function isValidDate(d) {
-    return d instanceof Date && !isNaN(d);
-  }
-
-  if (datas) {
-    const currentDate = new Date();
-    let dateFilter;
-
-    switch (datas) {
-      case '7dias':
-        dateFilter = new Date(currentDate.setDate(currentDate.getDate() - 7));
-        break;
-      case '1mes':
-        dateFilter = new Date(currentDate.setMonth(currentDate.getMonth() - 1));
-        break;
-      case '3meses':
-        dateFilter = new Date(currentDate.setMonth(currentDate.getMonth() - 3));
-        break;
-      case '6meses':
-        dateFilter = new Date(currentDate.setMonth(currentDate.getMonth() - 6));
-        break;
-      case '1ano':
-        dateFilter = new Date(currentDate.setFullYear(currentDate.getFullYear() - 1));
-        break;
-      default:
-        return res.status(400).json({
-          message: 'Filtro de datas inválido. Use "7dias", "1mes", "3meses", "6meses" ou "1ano".'
-        });
-    }
-
-    if (dateFilter && isValidDate(dateFilter)) {
-      sql += ` AND datas >= ?`;
-      values.push(dateFilter);
-    } else {
-      return res.status(400).json({
-        message: 'Data inválida fornecida.'
-      });
-    }
-  }
-
-  // Ordem pela data
-  if (orderByDate) {
-    if (orderByDate === 'asc') {
-      sql += ' ORDER BY datas ASC';
-    } else if (orderByDate === 'desc') {
-      sql += ' ORDER BY datas DESC';
-    } else {
-      return res.status(400).json({
-        message: 'Ordem de data inválida. Use "asc" ou "desc".'
-      });
-    }
-  }
-
-  connection.query(sql, values, (err, results) => {
+  connection.query(sqlInsert, values, (err, results) => {
     if (err) {
-      console.error('Erro ao buscar chamados:', err);
+      console.error('Erro ao adicionar chamado:', err);
       return res.status(500).send('Erro interno do servidor');
     }
 
-    res.status(200).json({
-      message: 'Consulta realizada com sucesso',
-      data: results
-    });
+    res.status(201).json({ message: 'Chamado adicionado com sucesso', chamadoId: results.insertId });
+  });
+};
+
+// Função para filtrar chamados
+const filtrarChamados = (connection) => (req, res) => {
+  const { filtro } = req.body;
+
+  const sqlFilter = 'SELECT * FROM abrirChamado WHERE setor = ?';
+  connection.query(sqlFilter, [filtro], (err, rows) => {
+    if (err) {
+      console.error('Erro ao filtrar chamados:', err);
+      return res.status(500).send('Erro interno do servidor');
+    }
+    res.json(rows);
   });
 };
 
 // Função para atualizar um chamado
-// utils.js
-const atualizarChamado = (connection) => async (req, res) => {
+const atualizarChamado = (connection) => (req, res) => {
   const { id } = req.params;
-  const { tiposDoChamado, nivelDeUrgencia, email, setor, nomeUsuario, nomeEquipamento, datas, FK_tecnicoResponsavelPeloChamado, descricao } = req.body;
+  const { nomeUsuario, datas, setor, tiposDoChamado, nivelDeUrgencia,
+    nomeEquipamento, FK_tecnicoResponsavelPeloChamado, email, descricao } = req.body;
 
-  const sql = `
-      UPDATE abrirChamado 
-      SET 
-          tiposDoChamado = ?, 
-          nivelDeUrgencia = ?, 
-          email = ?, 
-          setor = ?, 
-          nomeUsuario = ?, 
-          nomeEquipamento = ?, 
-          datas = ?, 
-          FK_tecnicoResponsavelPeloChamado = ?, 
-          descricao = ? 
-      WHERE id = ?`;
+  const sqlUpdate = `
+    UPDATE abrirChamado 
+    SET nomeUsuario = ?, datas = ?, setor = ?, tiposDoChamado = ?, 
+    nivelDeUrgencia = ?, nomeEquipamento = ?, FK_tecnicoResponsavelPeloChamado = ?, 
+    email = ?, descricao = ?
+    WHERE id = ?
+  `;
 
-  const values = [tiposDoChamado, nivelDeUrgencia, email, setor, nomeUsuario, nomeEquipamento, datas, FK_tecnicoResponsavelPeloChamado, descricao, id];
+  const values = [nomeUsuario, datas, setor, tiposDoChamado, nivelDeUrgencia,
+                  nomeEquipamento, FK_tecnicoResponsavelPeloChamado, email, descricao, id];
 
-  connection.query(sql, values, (error, results) => {
-      if (error) {
-          console.error(error);
-          return res.status(500).json({ message: 'Erro ao atualizar chamado.' });
-      }
-      if (results.affectedRows > 0) {
-          return res.status(200).json({ message: 'Chamado atualizado com sucesso!' });
-      } else {
-          return res.status(404).json({ message: 'Chamado não encontrado.' });
-      }
+  connection.query(sqlUpdate, values, (err, results) => {
+    if (err) {
+      console.error('Erro ao atualizar chamado:', err);
+      return res.status(500).send('Erro interno do servidor');
+    }
+    res.status(200).json({ message: 'Chamado atualizado com sucesso' });
   });
 };
-
 
 // Função para deletar um chamado
 const deletarChamado = (connection) => (req, res) => {
-  const chamadoId = req.params.id;
+  const { id } = req.params;
 
-  const sqlCheckIfExists = 'SELECT * FROM abrirChamado WHERE id = ?';
-  connection.query(sqlCheckIfExists, [chamadoId], (err, results) => {
+  const sqlDelete = 'DELETE FROM abrirChamado WHERE id = ?';
+  connection.query(sqlDelete, [id], (err, results) => {
     if (err) {
-      console.error('Erro ao verificar a existência do chamado:', err);
+      console.error('Erro ao deletar chamado:', err);
       return res.status(500).send('Erro interno do servidor');
     }
-
-    if (results.length === 0) {
-      return res.status(404).json({ message: 'Chamado não encontrado.' });
-    }
-
-    const sqlDelete = 'DELETE FROM abrirChamado WHERE id = ?';
-    connection.query(sqlDelete, [chamadoId], (err, result) => {
-      if (err) {
-        console.error('Erro ao deletar chamado', err);
-        return res.status(500).send('Erro interno do servidor');
-      }
-
-      res.status(200).send('Chamado deletado com sucesso');
-    });
+    res.status(200).json({ message: 'Chamado deletado com sucesso' });
   });
 };
 
-// Função para rota não existente
+// Função para lidar com rotas não existentes
 const rotaNaoExistente = (req, res) => {
-  res.status(404).send('Página não encontrada!');
+  res.status(404).json({ erro: 'Rota não encontrada' });
 };
 
+// Middleware para verificar o token JWT
+const verificarToken = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(403).json({ message: 'Acesso negado, token não fornecido.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'secreta-chave-jwt');
+    req.usuario = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Token inválido.' });
+  }
+};
+
+// Exportação das funções
 module.exports = {
   homeRoute,
   listarRegistros,
@@ -259,5 +216,7 @@ module.exports = {
   filtrarChamados,
   atualizarChamado,
   deletarChamado,
-  rotaNaoExistente
+  rotaNaoExistente,
+  loginUsuario,
+  verificarToken
 };
