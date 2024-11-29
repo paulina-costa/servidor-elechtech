820a1711f2e7b1e4c9f4c120955d328eccbd1dee
Rafael <rafael.silva.oliveira.tecnico@gmail.com>
2024-11-29 20:09:38 -0300

    renomeacao correta novamente do database

--- bdConfig.js
+++ bdConfig.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   port: 3306, 
   user: 'root', 
   password: 'root', 
-  database: 'siteweb'
+  database: 'siteWeb'
 });
 
 connection.connect((err) => {
