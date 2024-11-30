a224ba4935cda7e22a068012448bcd61ed80a7c1
Rafael <rafael.silva.oliveira.tecnico@gmail.com>
2024-11-29 21:25:04 -0300

    back pronto para aws

--- bdConfig.js
+++ bdConfig.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost', 
   port: 3306, 
   user: 'root', 
-  password: 'root', 
+  password: '', 
   database: 'siteWeb'
 });
 
